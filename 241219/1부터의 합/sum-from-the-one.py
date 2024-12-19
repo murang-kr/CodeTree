5c17a1445867f2cc67090f1c80ef9cece77646f4
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2024-12-19 23:05:29 +0900

    Title: 1부터의 합, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241219/1부터의 합/sum-from-the-one.py
+++ 241219/1부터의 합/sum-from-the-one.py
@@ -1,11 +1,7 @@
 n = int(input())
 sum_val = 0
 for i in range(1, 101):
-    if sum_val + i >= n:
-        break
     sum_val += i
-
-if sum_val == 0:
-    sum_val += n
-
-print(sum_val)
+    if sum_val + i >= n:
+        print(i)
+        break
