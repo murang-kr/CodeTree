3fe95a5125018bbb66ba2bb21252af131858d6aa
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2024-12-26 19:08:56 +0900

    Title: 별표 출력하기 12, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241226/별표 출력하기 12/print-star-12.py
+++ 241226/별표 출력하기 12/print-star-12.py
@@ -1,7 +1,7 @@
 n = int(input())
 for i in range(n):
     for j in range(n):
-        if i == 0 or j % 2 == 1:
+        if i == 0 or (j % 2 == 1 and j >= i):
             print("*", end = " ")
         else:
             print(" ", end = " ")
