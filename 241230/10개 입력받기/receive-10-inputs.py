21b1f3a83184ffcb53da4e5e6e45bb58ca791159
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2024-12-30 22:32:30 +0900

    Title: 10개 입력받기, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241230/10개 입력받기/receive-10-inputs.py
+++ 241230/10개 입력받기/receive-10-inputs.py
@@ -2,8 +2,8 @@ temp = list(map(int, input().split()))
 flag = False
 for i in range(len(temp)):
     if temp[i] == 0:
-        print(sum(temp[:i:]), sum(temp[:i:])/i)
+        print(sum(temp[:i:]), f"{sum(temp[:i:])/i:.1f}")
         flag = True
         break
 if not flag:
-    print(sum(temp), sum(temp)/len(temp))
+    print(sum(temp), f"{sum(temp)/len(temp):.1f}")
