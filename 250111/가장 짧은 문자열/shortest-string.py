f6b7f930014eb54fec7a3164deb01c6b51780603
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2025-01-11 16:19:43 +0900

    Title: 가장 짧은 문자열, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250111/가장 짧은 문자열/shortest-string.py
+++ 250111/가장 짧은 문자열/shortest-string.py
@@ -2,5 +2,5 @@ string_1 = input()
 string_2 = input()
 string_3 = input()
 
-arr = sorted([string_1, string_2, string_3])
-print(len(arr[0])-len(arr[2]))
+arr = sorted([string_1, string_2, string_3], key = lambda x: len(x))
+print(len(arr[2])-len(arr[0]))
