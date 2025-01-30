093f35baf2f3d68e847f0bcfaa75dc0b6b13724f
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2025-01-30 20:39:19 +0900

    Title: 함수를 이용한 온전수 판별, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250130/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+
+# Write your code here!
+
+def is_onjeonsu(n):
+    if n % 2 == 0:
+        return False
+    elif str(n)[len(str(n))-1] == "5":
+        return False
+    elif n % 3 == 0 and n % 9 != 0:
+        return False
+    else:
+        return True
+
+cnt = 0
+
+for i in range(a, b):
+    if is_onjeonsu(i):
+        cnt += 1
+print(cnt)
