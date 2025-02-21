32d531b3b974636b8521189090c0a41d410446b5
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2025-02-21 22:52:29 +0900

    Title: 국영수 순이지, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250221/국영수 순이지/korean-english-math-order.py
+++ 250221/국영수 순이지/korean-english-math-order.py
@@ -1,6 +1,6 @@
 n = int(input())
 arr = [tuple(input().split()) for _ in range(n)]
 
-arr.sort(key = lambda x: (x[1], x[2], x[3]), reverse = True)
+arr.sort(key = lambda x: (-int(x[1]), -int(x[2]), -int(x[3])))
 for name, kor, eng, math in arr:
     print(name, kor, eng, math)
