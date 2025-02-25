fa590b55a51eaabbc9ff6fdb041c13150f14e27f
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2025-02-25 17:15:41 +0900

    Title: DateTime to DateTime, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250225/DateTime to DateTime/datetime-to-datetime.py
+++ 250225/DateTime to DateTime/datetime-to-datetime.py
@@ -3,4 +3,7 @@ a, b, c = map(int, input().split())
 # Write your code here!
 the_day = a*24*60+b*60+c
 day = 11*24*60+11*60+11
-print(the_day-day)
+if the_day-day < 0:
+    print(-1)
+else:
+    print(the_day-day)
