26b151e24830fd151e8d89fdae772b1749d1ca6b
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2025-03-01 17:04:03 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 105ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 250301/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,30 @@
+x1, y1, x2, y2 = [0] * 2, [0] * 2, [0] * 2, [0] * 2
+x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
+x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
+
+# Please write your code here.
+grids = [[-1 for _ in range(2001)]for _ in range(2001)]
+OFFSET = 1000
+
+for i in range(2):
+    a, b, c, d = x1[i]+OFFSET, y1[i]+OFFSET, x2[i]+OFFSET, y2[i]+OFFSET
+    for j in range(a, c):
+        for k in range(b, d):
+            grids[j][k] = i
+
+max_x = 0
+min_x = 2000
+max_y = 0
+min_y = 2000
+for i in range(x1[0]+OFFSET, x2[0]+OFFSET):
+    for j in range(y1[0]+OFFSET, y2[0]+OFFSET):
+        if grids[i][j] == 0:
+            if i >= max_x and j >= max_y:
+                max_x, max_y = i, j
+            
+            if i <= min_x and j <= min_y:
+                min_x, min_y = i, j
+    
+max_x += 1
+max_y += 1
+print((max_x-min_x)*(max_y-min_y))
