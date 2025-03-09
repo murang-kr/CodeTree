d04f57162f61179f3693f3f84c1583b8b36d8b18
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2025-03-10 00:07:21 +0900

    Title: 거울에 레이저 쏘기 2, Time: 147ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 250310/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,35 @@
+n = int(input())
+grid = [list(input()) for _ in range(n)]
+k = int(input())
+
+# Please write your code here.
+dxr, dyr = [0, 1, 0, -1], [1, 0, -1, 0] #E, S, W, N
+dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1] #S, W, N, E
+rotate_num = 0
+dir_num = (k-1)//n
+x, y = 0, 0
+cnt = 0
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+for i in range(k-1):
+    nx, ny = x + dxr[rotate_num], y + dyr[rotate_num]
+    if in_range(nx, ny):
+        x, y = nx, ny
+    else:
+        rotate_num += 1
+
+while True:
+    if grid[x][y] == "/":
+        dir_num = int(dir_num + (-1 ** dir_num))
+    else:
+        dir_num = 3 - dir_num
+    nx, ny = x + dxs[dir_num], y + dys[dir_num]
+    cnt += 1
+    if in_range(nx, ny):
+        x, y = nx, ny
+    else:
+        break
+
+print(cnt)
