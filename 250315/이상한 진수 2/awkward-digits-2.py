2f07a73e035f652b7f22a91ff7a693336440a4c0
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2025-03-15 21:34:09 +0900

    Title: 이상한 진수 2, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250315/이상한 진수 2/awkward-digits-2.py
+++ 250315/이상한 진수 2/awkward-digits-2.py
@@ -1,10 +1,17 @@
 a = list(input())
 
 # Please write your code here.
+flag = True
 for i in range(len(a)):
     if a[i] == "0":
         a[i] = "1"
+        flag = False
         break
 
-a = "".join(a)
-print(int(a, 2))
+if flag:
+    a[-1] = "0"
+    a = "".join(a)
+    print(a)
+else:
+    a = "".join(a)
+    print(int(a, 2))
