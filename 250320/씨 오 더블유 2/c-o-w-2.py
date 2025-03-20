cce34bf50d02a76f937f31fd667f58123f8e1c02
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2025-03-20 12:55:20 +0900

    Title: 씨 오 더블유 2, Time: 57ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250320/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+string = input()
+
+# Please write your code here.
+cnt = 0
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if string[i] == "C" and string[j] == "O" and string[k] == "W":
+                cnt += 1
+
+print(cnt)
