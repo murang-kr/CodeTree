e86dc25a712bffef1be3df847439638b9c80acfb
Jo jaeguk <84118945+murang-kr@users.noreply.github.com>
2025-04-13 18:55:10 +0900

    Title: 밭의 높이를 고르게하기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250413/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 250413/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -3,7 +3,7 @@ arr = list(map(int, input().split()))
 
 # Please write your code here.
 cnt = 9999
-for i in range(N):
+for i in range(N-T+1):
     temp = arr[i:i+T]
     cost = 0
     for elem in temp:
